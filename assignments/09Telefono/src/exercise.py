0cda192cc6413da216ce13f611c11ea2c1471fe8
NolbertoTEC <A01641501@itesm.mx>
2021-08-18 14:57:36 +0000

    Tarea 1: Programas que realizan cálculos

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,9 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
-
+    mens = int(input("Dame el número de mensajes: "))
+    megs = float(input("Dame el número de megas: "))
+    mins = int(input("Dame el número de minutos: "))
+    costo = (mens*0.80)+(megs*0.80)+(mins*0.80)
+    print("El costo mensual es:", str(costo))
 
 if __name__ == '__main__':
     main()
